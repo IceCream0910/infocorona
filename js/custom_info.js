7d396f028c561df2e60481e9d46e7afef92e708d
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2020-03-18 18:52:15 +0900

    Update custom_info.js

--- js/custom_info.js
+++ js/custom_info.js
@@ -1,41 +1,4 @@
 
-$(function() {
-	
-    $('#results').empty()
-    keyWordsearch();
-  
-  
-  function keyWordsearch(){
-    gapi.client.setApiKey('AIzaSyDNWzGaX50QUdybwMKkcVYfMzGTqGZMlUs')
-    gapi.client.load('youtube', 'v3', function() {
-      makeRequest('코로나19')
-    })
-  }
-
-  function makeRequest(query) {
-    var request = gapi.client.youtube.search.list({
-      q: query,
-      part: 'snippet',
-      maxResults: 5
-    });
-
-    request.execute(function(response)  {
-      var srchItems = response.result.items;
-      for (var i = 0; i < srchItems.length; i++) {
-        var videoID = srchItems[i].id.videoId;
-        var video = $('<iframe allowfullscreen>');
-        video.attr('src', 'https://www.youtube.com/embed/' + videoID)
-        video.css({
-          border: 'none',
-          margin: '20px',
-          borderRadius: '15px'
-        })
-        $('#results').append(video)
-      }
-    })
-  }
-
-});
 
 angular.module("myApp", ['ngRoute'])
 
