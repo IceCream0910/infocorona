8e62195eeb01e689192f66e2615ee3bb3f9a2ba3
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2020-03-31 18:25:37 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,7 +49,14 @@
     }
 
 
-
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
 
 
 (function ($) {
