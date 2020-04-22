ddd943a68699823fada5b17cb6fc99526931eff4
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2020-04-22 10:28:21 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ $(window).scroll(function() {
             xAxes: [{
               display: false,
               categoryPercentage: 1,
-              barPercentage: 0.65
+              barPercentage: 0.7
             }],
             yAxes: [{
               display: false
