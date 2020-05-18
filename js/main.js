2e8393dd9cb5eb43d9938da19ebd68f3d0b85737
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2020-05-18 10:13:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -116,11 +116,11 @@ $(window).scroll(function() {
       var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ['5.7', '5.8', '5.9', '5.10', '5.11', '5.12', '5.13', '5.14', '5.15', '5.16'],
+          labels: ['5.8', '5.9', '5.10', '5.11', '5.12', '5.13', '5.14', '5.15', '5.16', '5.17'],
           datasets: [
             {
               label: "신규 확진자",
-              data: [12, 18, 34, 35, 27, 26, 29, 27, 19, 13],
+              data: [18, 34, 35, 27, 26, 29, 27, 19, 13, 15],
               borderColor: "transparent",
               borderWidth: "0",
               backgroundColor: "#ccc",
@@ -165,7 +165,7 @@ $(window).scroll(function() {
         type: 'doughnut',
         data: {
           datasets: [{
-            data: [19, 172, 467, 8, 500, 1],
+            data: [19, 178, 468, 8, 503, 1],
             backgroundColor: [
               '#FFA500',
               '#FFFF00',
@@ -222,7 +222,7 @@ $(window).scroll(function() {
         type: 'doughnut',
         data: {
           datasets: [{
-            data: [4549, 6501],
+            data: [4560, 6505],
             backgroundColor: [
               '#00b5e9',
               '#fa4251',
@@ -269,7 +269,7 @@ $(window).scroll(function() {
           datasets: [
             {
               label: "사망자수",
-              data: [0, 0, 0, 2, 3, 15, 38, 77, 127],
+              data: [0, 0, 0, 2, 3, 15, 38, 78, 127],
               borderColor: "rgba(0, 0, 0, 0.9)",
               borderWidth: "0",
               backgroundColor: "rgba(0, 0, 0, 0.5)"
@@ -319,7 +319,7 @@ $(window).scroll(function() {
           datasets: [
             {
               label: "확진자 수",
-              data: [147, 620, 3079, 1211, 1457, 1967, 1364, 715, 490],
+              data: [147, 621, 3082, 1215, 1462, 1968, 1365, 715, 490],
               borderColor: "rgba(255, 40, 0, 1)",
               borderWidth: "0",
               backgroundColor: "rgba(255, 40, 0, 0.8)"
@@ -365,11 +365,11 @@ $(window).scroll(function() {
         type: 'bar',
         defaultFontFamily: 'Poppins',
         data: {
-          labels: ['5.11', '5.12', '5.13', '5.14', '5.15', '5.16'],
+          labels: ['5.12', '5.13', '5.14', '5.15', '5.16', '5.17'],
           datasets: [
             {
               label: "확진",
-              data: [27, 26, 29, 27, 19, 13],
+              data: [26, 29, 27, 19, 13, 15],
               borderColor: "rgba(250, 66, 81, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(250, 66, 81,0.5)",
@@ -377,7 +377,7 @@ $(window).scroll(function() {
             },
             {
               label: "완치",
-              data: [38, 25, 67, 59, 30, 37],
+              data: [25, 67, 59, 30, 37, 16],
               borderColor: "rgba(0,173,95, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(0,173,95, 0.5)",
@@ -385,7 +385,7 @@ $(window).scroll(function() {
             },
             {
               label: "사망",
-              data: [2, 1, 1, 0, 2, 0],
+              data: [1, 1, 0, 2, 0, 1],
               borderColor: "rgba(0,0,0, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(0,0,0, 0.5)",
@@ -433,8 +433,8 @@ $(window).scroll(function() {
     const brandProduct2 = 'rgba(0,181,233,0.2)'
     const brandService2 = 'rgba(0,173,95,0.2)'
 
-    var data3 = [16330, 19579, 20722, 19875, 18843, 17660] //검사중
-    var data4 = [10922, 10962, 10991, 11018, 11037, 11050] //누적 확진
+    var data3 = [19579, 20722, 19875, 18843, 17660, 16093] //검사중
+    var data4 = [10962, 10991, 11018, 11037, 11050, 11065] //누적 확진
 
     var ctx = document.getElementById("recent-rep2-chart");
     if (ctx) {
@@ -442,7 +442,7 @@ $(window).scroll(function() {
       var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-          labels: ['5.11', '5.12', '5.13', '5.14', '5.15', '5.16'],
+          labels: ['5.12', '5.13', '5.14', '5.15', '5.16', '5.17'],
           datasets: [
             {
               label: '검사 중',    
