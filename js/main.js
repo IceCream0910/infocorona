5d4e9d50fa16cdc811b2c66299806b35530eb3da
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-08-15 23:47:23 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,16 +48,6 @@
     }
 
 
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-
-
 (function ($) {
   // USE STRICT
   "use strict";
