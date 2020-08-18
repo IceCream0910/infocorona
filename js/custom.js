911d12f51f251b7879140aa020aabd74c8a6f0f7
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-08-18 13:19:28 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -16,8 +16,12 @@ function getTTS() {
         var ttsdata4 = document.getElementById("curePM").innerHTML.replace("격리해제자", "").replace("(+", "").replace(")", "").replace(/\s/gi, "");
         var ttsdata5 = document.getElementById("death").innerHTML;
         var ttsdata6 = document.getElementById("deathPM").innerHTML.replace("사망자", "").replace("(+", "").replace(")", "").replace(/\s/gi, "");
-ttsurl = '국내 코로나일구 누적 확진자는 전일 0시 대비' + ttsdata2.toString() + '명 증가해 총 ' + ttsdata1.toString() + '이고, 격리해제자는 '+ ttsdata4.toString() + '명 증가해 총 '+ ttsdata3.toString() + '입니다. 사망자는 '+ttsdata6.toString() + '명 추가되어 총 '+ttsdata5.toString()+' 입니다. '
+        var ttsdata7 = document.getElementById("localConfirmed").innerHTML.replace("해외유입", "해외유입 사례는").replace('<br>', "이고, ").replace("국내발생", "국내발생 환자는")+"입니다.";
+
+ttsurl = '국내 코로나일구 누적 확진자는 전일 0시 대비' + ttsdata2.toString() + '명 증가해 총 ' + ttsdata1.toString() + '이고, 격리해제자는 '+ ttsdata4.toString() + '명 증가해 총 '+ ttsdata3.toString() + '입니다. 사망자는 '+ttsdata6.toString() + '명 추가되어 총 '+ttsdata5.toString()+' 입니다. 신규 환자 중 '+ttsdata7;
 document.getElementById("track").src="https://google-translate-proxy.herokuapp.com/api/tts?query="+ttsurl+"&language=ko&speed=1";
+document.getElementById("play-pause").style.color = "#556ee6";
+document.getElementById("play-pause").style.border = "solid 1.5px #556ee6";
 }
 
 var track = document.getElementById("track");
