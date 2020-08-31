e4a982c5bf29308afeb28b49364b4774bf41b0df
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-08-31 10:02:53 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -389,7 +389,7 @@ string2 = "2단계";
     const brandProduct2 = 'rgba(0,181,233,0.2)'
     const brandService2 = 'rgba(0,173,95,0.2)'
 
-    var data3 = [21415, 23669, 20073, 18138, 21612, 14841] //검사
+    var data3 = [23669, 20073, 18138, 21612, 14841, 13519] //검사
 
     var ctx = document.getElementById("recent-rep2-chart");
     if (ctx) {
@@ -397,7 +397,7 @@ string2 = "2단계";
       var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ["8.24", "8.25", "8.26", "8.27", "8.28", "8.29"],
+          labels: ["8.25", "8.26", "8.27", "8.28", "8.29", "8.30"],
           datasets: [
             {
               label: "일 검사자수",
