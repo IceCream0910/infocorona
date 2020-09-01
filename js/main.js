c2cf4e4958ef10eb2a68a45c88391a670c9e3d89
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-09-01 10:00:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,11 +72,11 @@ string2 = "2단계";
       var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ['8.21', '8.22', '8.23', '8.24', '8.25', '8.26', '8.27', '8.28', '8.29', '8.30'],
+          labels: ['8.22', '8.23', '8.24', '8.25', '8.26', '8.27', '8.28', '8.29', '8.30', '8.31'],
           datasets: [
             {
               label: "신규 확진자",
-              data: [332, 397, 266, 280, 320, 441, 371, 323, 299, 248],
+              data: [397, 266, 280, 320, 441, 371, 323, 299, 248, 235],
               borderColor: "transparent",
               borderWidth: "0",
               backgroundColor: "#ccc",
@@ -321,11 +321,11 @@ string2 = "2단계";
         type: 'bar',
         defaultFontFamily: 'Poppins',
         data: {
-          labels: ['8.25', '8.26', '8.27', '8.28', '8.29', '8.30'],
+          labels: ['8.26', '8.27', '8.28', '8.29', '8.30', '8.31'],
           datasets: [
             {
               label: "확진",
-              data: [320, 441, 371, 323, 299, 248],
+              data: [441, 371, 323, 299, 248, 235],
               borderColor: "rgba(250, 66, 81, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(250, 66, 81,0.5)",
@@ -333,7 +333,7 @@ string2 = "2단계";
             },
             {
               label: "완치",
-              data: [82, 93, 90, 214, 138, 70],
+              data: [93, 90, 214, 138, 70, 225],
               borderColor: "rgba(0,173,95, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(0,173,95, 0.5)",
@@ -341,7 +341,7 @@ string2 = "2단계";
             },
             {
               label: "사망",
-              data: [2, 1, 3, 5, 2, 1],
+              data: [1, 3, 5, 2, 1, 0],
               borderColor: "rgba(0,0,0, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(0,0,0, 0.5)",
@@ -389,7 +389,7 @@ string2 = "2단계";
     const brandProduct2 = 'rgba(0,181,233,0.2)'
     const brandService2 = 'rgba(0,173,95,0.2)'
 
-    var data3 = [23669, 20073, 18138, 21612, 14841, 13519] //검사
+    var data3 = [20073, 18138, 21612, 14841, 13519, 21391] //검사
 
     var ctx = document.getElementById("recent-rep2-chart");
     if (ctx) {
@@ -397,7 +397,7 @@ string2 = "2단계";
       var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ["8.25", "8.26", "8.27", "8.28", "8.29", "8.30"],
+          labels: ["8.26", "8.27", "8.28", "8.29", "8.30", "8.31"],
           datasets: [
             {
               label: "일 검사자수",
