4ad6a7d31f7be8870ebb6ed05694ecd20962ce7d
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-09-06 10:03:45 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,11 +72,11 @@ string2 = "2단계";
       var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ['8.26', '8.27', '8.28', '8.29', '8.30', '8.31', '9.1', '9.2', '9.3', '9.4'],
+          labels: ['8.27', '8.28', '8.29', '8.30', '8.31', '9.1', '9.2', '9.3', '9.4', '9.5'],
           datasets: [
             {
               label: "신규 확진자",
-              data: [441, 371, 323, 299, 248, 235, 267, 195, 198, 168],
+              data: [371, 323, 299, 248, 235, 267, 195, 198, 168, 167],
               borderColor: "transparent",
               borderWidth: "0",
               backgroundColor: "#ccc",
@@ -321,11 +321,11 @@ string2 = "2단계";
         type: 'bar',
         defaultFontFamily: 'Poppins',
         data: {
-          labels: ['8.30', '8.31', '9.1', '9.2', '9.3', '9.4'],
+          labels: ['8.31', '9.1', '9.2', '9.3', '9.4', '9.5'],
           datasets: [
             {
               label: "확진",
-              data: [248, 235, 267, 195, 198, 168],
+              data: [235, 267, 195, 198, 168, 167],
               borderColor: "rgba(250, 66, 81, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(250, 66, 81,0.5)",
@@ -333,7 +333,7 @@ string2 = "2단계";
             },
             {
               label: "완치",
-              data: [70, 225, 158, 173, 254, 226],
+              data: [225, 158, 173, 254, 226, 137],
               borderColor: "rgba(0,173,95, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(0,173,95, 0.5)",
@@ -341,7 +341,7 @@ string2 = "2단계";
             },
             {
               label: "사망",
-              data: [1, 0, 2, 3, 2, 2],
+              data: [0, 2, 3, 2, 2, 1],
               borderColor: "rgba(0,0,0, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(0,0,0, 0.5)",
