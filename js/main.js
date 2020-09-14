b927603bca5e349a9b007586cc83f188b644c355
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-09-14 14:56:12 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -178,7 +178,7 @@ string2 = "2단계";
         type: 'doughnut',
         data: {
           datasets: [{
-            data: [10063, 11992],
+            data: [10177, 12108],
             backgroundColor: [
               '#00b5e9',
               '#fa4251',
@@ -225,7 +225,7 @@ string2 = "2단계";
           datasets: [
             {
               label: "사망자수",
-              data: [0, 0, 0, 2, 4, 17, 42, 109, 181],
+              data: [0, 0, 0, 2, 4, 17, 42, 113, 185],
               borderColor: "rgba(0, 0, 0, 0.9)",
               borderWidth: "0",
               backgroundColor: "rgba(0, 0, 0, 0.5)"
@@ -275,7 +275,7 @@ string2 = "2단계";
           datasets: [
             {
               label: "확진자 수",
-              data: [526, 1230, 4543, 2713, 2946, 4061, 3425, 1710, 901],
+              data: [532, 1236, 4584, 2738, 268, 4104, 3481, 1734, 908],
               borderColor: "rgba(255, 40, 0, 1)",
               borderWidth: "0",
               backgroundColor: "rgba(255, 40, 0, 0.8)"
