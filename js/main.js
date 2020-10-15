7f7c4ae00ee2521147c06c3ab46a04b55be0e2bf
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-10-15 15:47:29 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -173,7 +173,7 @@
         type: 'doughnut',
         data: {
           datasets: [{
-            data: [11594, 13295],
+            data: [11615, 13373],
             backgroundColor: [
               '#00b5e9',
               '#fa4251',
@@ -220,7 +220,7 @@
           datasets: [
             {
               label: "사망자수",
-              data: [0, 0, 0, 2, 4, 21, 47, 143, 221],
+              data: [0, 0, 0, 2, 4, 21, 47, 143, 222],
               borderColor: "rgba(0, 0, 0, 0.9)",
               borderWidth: "0",
               backgroundColor: "rgba(0, 0, 0, 0.5)"
@@ -270,7 +270,7 @@
           datasets: [
             {
               label: "확진자 수",
-              data: [623, 1369, 4942, 3060, 3318, 4583, 3970, 2008, 1016],
+              data: [628, 1372, 4955, 3070, 3326, 4588, 3983, 2019, 1047],
               borderColor: "rgba(255, 40, 0, 1)",
               borderWidth: "0",
               backgroundColor: "rgba(255, 40, 0, 0.8)"
