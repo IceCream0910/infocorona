ef81c6062ff45191215c54dda7aaed9dadeceba9
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-10-25 15:21:55 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-   
+$( window ).scroll( function() {
+  if ( $( this ).scrollTop() > 200 ) {
+    $( '.top' ).fadeIn();
+  } else {
+    $( '.top' ).fadeOut();
+  }
+} );
+
+$( '.top' ).click( function() {
+  $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+  return false;
+} );
+
 
    $(document).ready(function(){       
         
