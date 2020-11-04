5e0213bdb65c9bc35d5539c2fd214a0c8f4d406e
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-11-04 09:48:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,11 +80,11 @@ $( '.top' ).click( function() {
       var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ['10.24', '10.25', '10.26', '10.27', '10.28', '10.29', '10.30', '10.31', '11.1', '11.2'],
+          labels: ['10.25', '10.26', '10.27', '10.28', '10.29', '10.30', '10.31', '11.1', '11.2', '11.3'],
           datasets: [
             {
               label: "신규 확진자",
-              data: [61, 119, 88, 103, 125, 114, 127, 124, 97, 75],
+              data: [119, 88, 103, 125, 114, 127, 124, 97, 75, 118],
               borderColor: "transparent",
               borderWidth: "0",
               backgroundColor: "#ccc",
@@ -186,11 +186,11 @@ $( '.top' ).click( function() {
         type: 'bar',
         defaultFontFamily: 'Poppins',
         data: {
-          labels: ['10.28', '10.29', '10.30', '10.31', '11.1', '11.2'],
+          labels: ['10.29', '10.30', '10.31', '11.1', '11.2', '11.3'],
           datasets: [
             {
               label: "확진",
-              data: [125, 114, 127, 124, 97, 75],
+              data: [114, 127, 124, 97, 75, 118],
               borderColor: "rgba(250, 66, 81, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(250, 66, 81,0.5)",
@@ -198,7 +198,7 @@ $( '.top' ).click( function() {
             },
             {
               label: "완치",
-              data: [95, 59, 84, 46, 38, 115],
+              data: [59, 84, 46, 38, 115, 106],
               borderColor: "rgba(0,173,95, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(0,173,95, 0.5)",
@@ -206,7 +206,7 @@ $( '.top' ).click( function() {
             },
             {
               label: "사망",
-              data: [1, 1, 1, 2, 2, 4],
+              data: [1, 1, 2, 2, 4, 2],
               borderColor: "rgba(0,0,0, 0.8)",
               borderWidth: "0",
               backgroundColor: "rgba(0,0,0, 0.5)",
