f2e4ecbb8a2ca21a51cd679c89ef4b52ee788506
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-11-25 09:42:14 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@ $( '.top' ).click( function() {
     const brandProduct2 = 'rgba(0,181,233,0.2)'
     const brandService2 = 'rgba(0,173,95,0.2)'
 
-    var data3 = [19481, 19600, 23303, 12144, 13245, 24264] //검사
+    var data3 = [19600, 23303, 12144, 13245, 24264, 20006] //검사
 
     var ctx = document.getElementById("recent-rep2-chart");
     if (ctx) {
@@ -262,7 +262,7 @@ $( '.top' ).click( function() {
       var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ["11.18", "11.19", "11.20", "11.21", "11.22", "11.23"],
+          labels: ["11.19", "11.20", "11.21", "11.22", "11.23", "11.24"],
           datasets: [
             {
               label: "일 검사자수",
@@ -311,7 +311,7 @@ $( '.top' ).click( function() {
     const brandProduct2 = 'rgba(0,181,233,0.2)'
     const brandService2 = 'rgba(0,173,95,0.2)'
 
-    var data3 = [1.76, 1.85, 1.66, 2.72, 2.05, 1.44] //  (확진수)/(검사수)*100
+    var data3 = [1.85, 1.66, 2.72, 2.05, 1.44, 1.91] //  (확진수)/(검사수)*100
 
     var ctx = document.getElementById("tests-percent-chart");
     if (ctx) {
@@ -319,7 +319,7 @@ $( '.top' ).click( function() {
       var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ["11.18", "11.19", "11.20", "11.21", "11.22", "11.23"],
+          labels: ["11.19", "11.20", "11.21", "11.22", "11.23", "11.24"],
           datasets: [
             {
               label: "검사 대비 확진율",
