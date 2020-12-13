830509f3ec3d2a428a7a91020a3c43e86b5ea916
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-12-13 14:40:17 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+$("document").ready(function(){
+  $(".tab-slider--body").hide();
+  $(".tab-slider--body:first").show();
+  $(".tab-slider--body2").hide();
+  $(".tab-slider--body2:first").show();
+});
+
+$(".tab-slider--nav li").click(function() {
+  $(".tab-slider--body").hide();
+  var activeTab = $(this).attr("rel");
+  $("#"+activeTab).fadeIn();
+  if($(this).attr("rel") == "tab2"){
+    $('.tab-slider--tabs').addClass('slide');
+  }else{
+    $('.tab-slider--tabs').removeClass('slide');
+  }
+  $(".tab-slider--nav li").removeClass("active");
+  $(this).addClass("active");
+});
+
+
+$(".tab-slider--nav2 li").click(function() {
+  $(".tab-slider--body2").hide();
+  var activeTab = $(this).attr("rel");
+  $("#"+activeTab).fadeIn();
+  if($(this).attr("rel") == "tab4"){
+    $('.tab-slider--tabs2').addClass('slide');
+  }else{
+    $('.tab-slider--tabs2').removeClass('slide');
+  }
+  $(".tab-slider--nav2 li").removeClass("active");
+  $(this).addClass("active");
+});
+
+
 $( window ).scroll( function() {
   if ( $( this ).scrollTop() > 200 ) {
     $( '.top' ).fadeIn();
