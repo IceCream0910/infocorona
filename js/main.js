08641a076a00b417d19f058a433b57afae932c68
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2020-12-21 09:30:55 +0900

    201221 | 사전 업데이트

--- js/main.js
+++ js/main.js
@@ -110,15 +110,15 @@ function disableSelection(target) {
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['12.13', '12.14', '12.15', '12.16', '12.17', '12.18', '12.19'],
+                labels: ['12.14', '12.15', '12.16', '12.17', '12.18', '12.19', '12.20'],
                 datasets: [{
                     label: '국내 발생',
                     backgroundColor: "#778beb",
-                    data: [682, 848, 1054, 993, 1036, 1029, 1072],
+                    data: [848, 1054, 993, 1036, 1029, 1072, 892],
                 }, {
                     label: '해외 유입',
                     backgroundColor: "#a9a9a9",
-                    data: [36, 32, 24, 21, 26, 24, 25],
+                    data: [32, 24, 21, 26, 24, 25, 34],
                 }],
             },
             options: {
@@ -165,24 +165,24 @@ function disableSelection(target) {
                 type: 'bar',
                 defaultFontFamily: 'Poppins',
                 data: {
-                    labels: ['12.14', '12.15', '12.16', '12.17', '12.18', '12.19'],
+                    labels: ['12.15', '12.16', '12.17', '12.18', '12.19', '12.20'],
                     datasets: [{
                             label: "확진",
-                            data: [880, 1078, 1014, 1062, 1052, 1097],
+                            data: [1078, 1014, 1062, 1052, 1097, 926],
                             borderWidth: "0",
                             backgroundColor: "rgba(250, 66, 81,0.5)",
                             fontFamily: "Poppins"
                         },
                         {
                             label: "완치",
-                            data: [457, 388, 663, 372, 352, 388],
+                            data: [388, 663, 372, 352, 388, 433],
                             borderWidth: "0",
                             backgroundColor: "rgba(0,173,95, 0.5)",
                             fontFamily: "Poppins"
                         },
                         {
                             label: "사망",
-                            data: [13, 12, 22, 11, 14, 15],
+                            data: [12, 22, 11, 14, 15, 24],
                             borderWidth: "0",
                             backgroundColor: "rgba(0,0,0, 0.5)",
                             fontFamily: "Poppins"
@@ -246,7 +246,7 @@ function disableSelection(target) {
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: ['12.14', '12.15', '12.16', '12.17', '12.18', '12.19'],
+                labels: ['12.15', '12.16', '12.17', '12.18', '12.19', '12.20'],
                 datasets: [{
                     label: "위중증 환자",
                     borderColor: gradientStroke,
@@ -261,7 +261,7 @@ function disableSelection(target) {
                     fill: true,
                     backgroundColor: gradientFill,
                     borderWidth: 4,
-                    data: [205, 226, 242, 246, 275, 278]
+                    data: [226, 242, 246, 275, 278, 274]
                 }]
             },
             options: {
